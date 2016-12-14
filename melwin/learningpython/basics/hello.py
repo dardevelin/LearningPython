5b36ba87d90aa02a4f8823fb7cef97d6c0f1ed9c
MELWIN JOSE <melwin.jose.1991@gmail.com>
2016-12-14 10:42:44 -0500

    first py commit

---
+++ melwin/learningpython/basics/hello.py
@@ -0,0 +1,7 @@
+print("Hello World!!!")
+
+x=0
+if x==1:
+    print("x is 1")
+else:
+    print("x is not 1")
---
+++ melwin/learningpython/basics/variables.py
@@ -0,0 +1,25 @@
+## Python is completely object oriented, and not "statically typed". 
+## You do not need to declare variables before using them, or declare 
+## their type. Every variable in Python is an object.
+
+
+## Numbers
+# Python supports two types of numbers - integers and floating point numbers
+
+my_int = 7
+my_float = 7.0
+my_float_2 = float(7.0)
+
+print(my_int," ",my_float," ",my_float_2)
+
+
+## Strings
+string_1 = 'it is string_1'
+string_2 = "it's string_2"
+print(string_1+" & "+string_2)
+
+
+## Assingment
+a,b = 3,4
+print(a," ",b)
+
